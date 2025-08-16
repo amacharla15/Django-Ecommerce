5a9d70eb190b1af21c49cbd20aada3664d5d976f
amacharla15 <amacharla@csuchico.edu>
2025-08-15 22:09:23 -0700

    unordered data, queryset data manipulation

--- products/views.py
+++ products/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products
 # Create your views here.
 
 def hello(request):
+    data=Products.objects.all().order_by('id')
+    
+    print(data)
+    if(data.exists()):
+        data.name="abc"
+        data.save()
     return HttpResponse("Hello World")
